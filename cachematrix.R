e8d94830effbf678fa968010ba91ff63be892ecf
rpizarrog <rpizarro@itdurango.edu.mx>
2016-07-03 11:31:27 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function receive a matrix like parameter
+## This parameter is presented 
+## this funcitoncontain two functions
+## fist of them: Assign the matrix, obtaining new matrix
+## Secon of them: # only return matrix from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  setMat <- function() {
+    mati <<- x # Assign the matrix, obtaining new matrix
+  }
+  
+  getMat <- function() {
+    return (mati) # only return matrix from cache
+  }
+  
+  setInverseMat<-function() {
+    inverseMati <<-solve(x)
+    #inserseMati <- solve(InverseMati)
+  }
+  
+  getInverseMat <- function() {
+    print ("Matrix from cache")
+    print (mati)
+    print ("Returning inverse from cache")
+    return (inverseMati) # only return inverse from cache
+  }
+  
+  
+  list(setMat=setMat, getMat=getMat, setInverseMat=setInverseMat, getInverseMat=getInverseMat)  # generate the list
 }
 
-
-## Write a short comment describing this function
-
+## This function generate the inverse of a matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (!is.null(makeCacheMatrix()$getMat())) {
+    # Obtaing de matrix inverse from matrix cache using solve()
+    print ("Obtaining inverse matrix from cache")
+    inverseMat <- makeCacheMatrix()$getInverseMat()
+    return (inverseMat)
+  }
+  
+  mat<-matrix(sample(1:(x*x)), nrow=x, ncol=x) # new random sqare matrix 
+  makeCacheMatrix(mat)$setMat()  # setting new matrix
+  makeCacheMatrix(mat)$setInverseMat()  # setting new inverse matrix
+                
 }
